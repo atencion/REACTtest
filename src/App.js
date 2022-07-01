2128e9e735b0d20ee6407202e4bcac897cf19caa
atencion <maxmaxmax@gmail.com>
2022-07-01 15:09:18 -0300

    fantastic

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some Max magic happen!</h2>
       <h3>Now everything is in h3 via GIT</h3>
+      <h3>FANTASTIC</h3>
     </div>
   );
 }
